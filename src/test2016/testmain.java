ac856908278f4421705d36d5ca7324e2b975e02e
showMeLiang <lda_ming@163.com>
2016-12-30 16:43:09 +0800

    333

--- src/test2016/testmain.java
+++ src/test2016/testmain.java
@@ -3,7 +3,7 @@ package test2016;
 public class testmain {
 	public static void main(String[] args) {
 		//在这里 写代码
-		  System.out.println("你好!!!");//  
+		  System.out.println("你好!!！！");//  
 		}
 		
 
